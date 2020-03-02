1255ff9db01844084faa310f10bfb093411d611f
William Lin <wlin01@stuy.edu>
2020-03-02 09:23:00 -0500

    updated

--- populate.py
+++ populate.py
@@ -5,13 +5,12 @@ import json
 import datetime
 
 client = MongoClient()
-db = client.test
-restaurants = db.restaurants
-restaurants.delete_many({})
+db = client.nasa
+meteorites = db.meteorites
+meteorites.delete_many({})
 
-with open("./primer-dataset.json", 'r') as file:
+with open("meteorites.json", 'r') as file:
     data = json.load(file)
     for member in data:
-        id = restaurants.insert_one(loads(json.dumps(member)))
+        id = meteorites.insert_one(loads(json.dumps(member)))
         print(id)
-
