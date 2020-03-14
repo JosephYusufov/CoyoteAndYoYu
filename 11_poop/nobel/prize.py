5b5176e0ccf35570302f858c87d3bf3430dab3dc
Derek Leung <dleung00@stuy.edu>
2020-03-14 12:17:26 -0400

    hi

--- 11_poop/nobel/prize.py
+++ 11_poop/nobel/prize.py
@@ -54,8 +54,6 @@ def findTopic(topic):
             elif(item!="_id"):
                 count = 0
                 for piece in thing[item]:
-                    #print(piece)
-                    #print(thing[item][piece])
                     infoDict = thing[item][piece]
                     if(count == 0):
                         hello.append("reason: " + infoDict[u'motivation'])
@@ -73,12 +71,14 @@ def findYear(year):
             if (item != "laureates" and item!="_id"):
                 hello.append("{} : {}".format(item, thing[item]))
             elif(item!="_id"):
+                count = 0
                 for piece in thing[item]:
-                   # print(thing[item][piece])
                     infoDict = thing[item][piece]
-                    hello[-1]+=("name: " + infoDict['firstname'])
-                    hello[-1]+=("reason: " + infoDict[u'motivation'])
-        hello[-1]+=("\n")
+                    if(count == 0):
+                        hello.append("reason: " + infoDict[u'motivation'])
+                    hello.append("name: " + infoDict['firstname'])
+                    count+=1
+        hello.append("\n")
     return hello
 
 #findTopic("chemistry")
